4b7c508f2868932a1e441ecb07c967616d04e403
findthenapp <findthenapp@gmail.com>
2016-05-08 22:40:39 -0400

    first commit

---
+++ MakeFixedLength/MakeItFixedLength.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MakeFixedLength
+{
+    partial class MakeItFixedLength
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Browse = new System.Windows.Forms.Button();
+            this.btn_Convert = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbCharLength = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(358, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Path";
+            // 
+            // btn_Browse
+            // 
+            this.btn_Browse.Location = new System.Drawing.Point(491, 40);
+            this.btn_Browse.Name = "btn_Browse";
+            this.btn_Browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_Browse.TabIndex = 2;
+            this.btn_Browse.Text = "Browse";
+            this.btn_Browse.UseVisualStyleBackColor = true;
+            this.btn_Browse.Click += new System.EventHandler(this.btn_Browse_Click);
+            // 
+            // btn_Convert
+            // 
+            this.btn_Convert.Location = new System.Drawing.Point(260, 94);
+            this.btn_Convert.Name = "btn_Convert";
+            this.btn_Convert.Size = new System.Drawing.Size(75, 23);
+            this.btn_Convert.TabIndex = 3;
+            this.btn_Convert.Text = "Convert";
+            this.btn_Convert.UseVisualStyleBackColor = true;
+            this.btn_Convert.Click += new System.EventHandler(this.btn_Convert_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Character Length";
+            // 
+            // tbCharLength
+            // 
+            this.tbCharLength.Location = new System.Drawing.Point(127, 68);
+            this.tbCharLength.Name = "tbCharLength";
+            this.tbCharLength.Size = new System.Drawing.Size(60, 20);
+            this.tbCharLength.TabIndex = 4;
+            this.tbCharLength.Text = "432";
+            // 
+            // MakeItFixedLength
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 165);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbCharLength);
+            this.Controls.Add(this.btn_Convert);
+            this.Controls.Add(this.btn_Browse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MakeItFixedLength";
+            this.Text = "Make It Fixed Length";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Browse;
+        private System.Windows.Forms.Button btn_Convert;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbCharLength;
+    }
+}
+
---
+++ MakeFixedLength/MakeItFixedLength.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MakeFixedLength
+{
+    public partial class MakeItFixedLength : Form
+    {
+        public MakeItFixedLength()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String outputFilePath = Path.Combine(Path.GetDirectoryName(textBox1.Text), "Output", Path.GetFileName(textBox1.Text));
+                Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
+                if (!String.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    Int32 charLength = 0;
+                    if (String.IsNullOrWhiteSpace(tbCharLength.Text))
+                    {
+                        MessageBox.Show("Please enter character length");
+                        return;
+                    }
+                    else
+                    {
+                        charLength = Convert.ToInt32(tbCharLength.Text);
+                    }
+                    ConvertFileToFixedLength(outputFilePath, charLength);
+                }
+                else
+                    MessageBox.Show("Please select a file");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void ConvertFileToFixedLength(String outputFilePath, Int32 charLength)
+        {            
+            using (StreamReader reader = new StreamReader(textBox1.Text))
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                string line;
+                bool isFirstLine = true;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!isFirstLine)
+                        writer.Write(Environment.NewLine);
+                    else
+                        isFirstLine = false;
+
+                    if (line.Length >= charLength)
+                    {
+                        writer.Write(line);
+                    }
+                    else
+                    {
+                        writer.Write(line + new String(' ', charLength - line.Length));
+                    }
+                }
+            }
+            MessageBox.Show("Converted file available @ "+outputFilePath);
+        }
+
+        private void btn_Browse_Click(object sender, EventArgs e)
+        {            
+            DialogResult result = openFileDialog1.ShowDialog(); 
+            if (result == DialogResult.OK) // Test result.
+            {
+               textBox1.Text = openFileDialog1.FileName;               
+            }
+        }
+    }
+}
